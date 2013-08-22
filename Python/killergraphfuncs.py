99dbb646e87e2ec50964f488e42404bf0b170cae
chase <cc7768@gmail.com>
2013-08-22 15:00:10 -0400

    Added doublex9 to killergraphfuncs.py

--- Python/killergraphfuncs.py
+++ Python/killergraphfuncs.py
@@ -8,7 +8,7 @@ Date: 2013-08-22 10:09:04
 from __future__ import division, print_function
 from math import sqrt
 import numpy as np
-from scipy.linalg import solve, eig
+from scipy.linalg import solve, eig, norm, inv, lstsq
 
 
 def doublej(a1, b1, max_it=50):
@@ -133,6 +133,94 @@ def doubleo(A, C, Q, R, tol=1e-15):
 
     return k1, g1
 
+def doublex9(A, B, D, Q, R, sig):
+    '''
+    function[F,K,P,Pt]=doublex9(A,B,D,Q,R,sig)
+    Revised Feb 15, 2001
+    Solves the undiscounted robust control problem
+
+        min sum (x'Qx + u'R u) for the state space system
+
+        x' = A x + B u + D w
+        sig < 0 indicates preference for robustness
+        sig = -1/theta where theta is the robustness multiplier.
+
+    Please note that because this is a MINIMUM problem, the convention
+    is that Q and R are `positive definite' matrices (subject to the usual
+    detectability qualifications).
+    The optimal control with observed state is
+        u_t = - F x_t
+
+    The value function is -x'Px; note that the program returns a positive
+    semi-definite P. Pt is the matrix D(P) where D(.) is the operator
+    described in Hansen-Sargent.  The conservative measure
+    of continuation value is -y' D(P) y = -y' Pt y,
+    where y=x', next period's state.
+    The program also returns a worst-case shock  matrix K
+    where w_{t+1} = K x_t is the worst case shock.
+
+    For problems with a target vector minimization
+      min sum (z'z) with
+       z  = H x + G u
+    we have  R = G'G,   Q = H'H, and require H'G=0.
+    Note that this is a minimum problem.
+    If you want to combine this with robust estimation of
+    the state, also use doublex8 to compute altered rule Fbar.
+    doublex9 uses doubling algorithm from Anderson, et. al.
+    Initializes with po=I.  The version is designed to find the optimal
+    stable work solution in, for example,
+    `permanent income' models that don't satisfy the detectability condition.
+    See AHMS (Computational economics handbook chapter) for details.  doublex9
+    adapts the AHMS algorithm to the risk-sensitive specicfication of
+    Hansen-Sargent (IEEE, May, 1995)
+    '''
+    # Need to make sure R is an array
+    if type(R) != np.ndarray:
+        R = np.array([[R]])
+
+
+    tol=1e-15
+    dd=1
+
+    ss = max(A.shape)
+    v = np.eye(ss)
+    po = v
+    J = np.dot(B, solve(R, B.T)) + sig * np.dot(D, D.T)
+    t = v + np.dot(J, po)
+
+    a0 = solve(t, A)
+    b0 = solve(t, J)
+    g0 = Q - po + np.dot(A.T, po.dot(a0))  # note Q is H'*H
+
+    while dd>tol:
+        a1 = np.dot(a0, solve((v + b0.dot(g0)), a0))
+        b1 = b0 + np.dot(a0, solve(v+b0.dot(g0), np.dot(b0, a0.T)))
+        g1 = g0 + np.dot(a0.T, g0.dot(solve((v+b0.dot(g0)), a0)))
+        dd = norm(g1 - g0) / norm(g0)
+        a0 = a1
+        b0 = b1
+        g0 = g1
+
+    gg1 = g1 + po   # gg1=g1+po  (changed from  paper)
+    P = gg1
+    # F=B'*P*((v+J*P)\A)    original formula from warehouse
+    # g1t=P/(v+sig*P*D*D')  Change of sign to match the doublex4.m convention
+
+    #  this line is taken from doublex4.m to take care of
+    g1t = solve(v + np.dot(sig, gg1.dot(D.dot(D.T))), gg1)
+    # transpose problem corrected april 14, 2000
+
+
+    F = solve(np.dot(B.T, g1t.dot(B)) + R, np.dot(B.T, g1t.dot(A)))
+    Pt = g1t
+    rD, cD = D.shape
+    K = -sig * (inv(np.eye(cD) + sig * np.dot(D.T, P.dot(D))).dot(
+        np.dot(D.T, P.dot(A - B.dot(F)))))
+
+    return F, K, P, Pt
+
+
+
 
 def olrp(beta, A, B, Q, R, W=None, tol=1e-6, max_iter=1000):
     """
@@ -237,3 +325,4 @@ def olrp(beta, A, B, Q, R, W=None, tol=1e-6, max_iter=1000):
         p = p1
 
     return f, p
+
